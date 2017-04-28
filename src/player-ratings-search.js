004973222322d088a57d215b40f68fe4310a5824
Quinn <qbakerim@gmail.com>
2017-04-28 12:08:36 -0500

    draft 2 of website

--- src/player-ratings-search.js
+++ src/player-ratings-search.js
@@ -4,6 +4,22 @@
  * 3) Append the html from step 2 to the results div
  */
 
+var playerArray = ["Quinn Baker", "Mighy Moose", "Brian Bae", "Kenta Matusdaira", "Dimitrij Ovtcharov"];
+var ratingArray = ["1777", "1234", "2000", "2801", "2799"];
+for(var i = 0; i < playerArray.length; i++) {
+    var node = document.createElement("li"); 
+    var textnode = document.createTextNode(search);
+    node.appendChild(textnode); 
+    document.getElementById("playerArray").appendChild(node);
+}
+
+for(var i = 0; i < ratingArray.length; i++) {
+    var node = document.createElement("li"); 
+    var textnode = document.createTextNode(search);
+    node.appendChild(textnode); 
+    document.getElementById("ratingArray").appendChild(node);
+}
+
 var results = document.querySelector('#results');
 var search = document.querySelector('#search');
 
@@ -17,11 +33,35 @@ results.appendChild(userDiv);
 // Get players from the server
 fetch('player-ratings.json').then(function(results){
     results.json().then(function(users) {
-        alert(JSON.stringify(users));
+        fetch(JSON.stringify(users));
     });
 });
 
 // React to input changes
-search.addEventListener('keyup', function() {
+mainSearch.addEventListener('keyup', function() {
     alert(search.value);
 });
+
+citySearch.addEventListener('keyup', function() {
+    fetch(search.value);
+});
+
+stateSearch.addEventListener('keyup', function() {
+    fetch(search.value);
+});
+
+ratingSearch1.addEventListener('keyup', function() {
+    fetch(search.value);
+});
+
+ratingSearch2.addEventListener('keyup', function() {
+    fetch(search.value);
+});
+
+/*give player list and rating list ids
+get rid of names in the lis
+create an array which list of random players names
+loop through the list of names
+also loop through the ratings
+for each name, create an li element containing the name and append it to the name list
+*/
